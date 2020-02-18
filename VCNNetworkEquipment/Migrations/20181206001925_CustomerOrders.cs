c3a1bed17c31949a943fbc2d378157dda59643b5
Calvin05 <cuongnguyen9505@gmail.com>
2020-02-17 21:29:46 -0500

    push project

---
+++ VCNNetworkEquipment/Migrations/20181206001925_CustomerOrders.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace VCNNetworkEquipment.Migrations
+{
+    public partial class CustomerOrders : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Customer",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Customer",
+                table: "Orders");
+        }
+    }
+}
